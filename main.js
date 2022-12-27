2fa590876a46cd37d3bfd32d0f07c29b63ecca68
claytonfortunato <104373308+claytonfortunato@users.noreply.github.com>
2022-12-27 18:55:14 -0300

    att

--- main.js
+++ main.js
@@ -1,32 +1,69 @@
-const AddButton = document.querySelector(".task-item");
-const todoInput = document.querySelector(".todo-input");
-const todoList = document.querySelector(".new-task-container");
+const AddButton = document.querySelector(".new-task-button");
+const todoInput = document.querySelector(".new-task-input");
 
-const saveTodo = (text) => {
-  const todo = document.querySelector("div");
-  todo.classList.add(".new-task-container");
+const tasksContainer = document.querySelector(".new-task-container");
 
-  const todoTitle = document.querySelector("p");
-  todoTitle.innerText = text;
-  todo.appendChild(todoTitle);
+const validateInput = () => todoInput.value.trim().length > 0;
 
-  const deleteBtn = document.querySelector("button");
-  deleteBtn.classList.add("#deleteBtn");
-  deleteBtn.innerHTML = `<i class="far fa-trash-alt"></i>`;
-  todo.appendChild(deleteBtn);
+const handleAddTask = () => {
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("tasks-item");
 
-  todoList.appendChild(todo);
-  console.log(todoList);
+  const taskContent = document.createElement("p");
+  taskContent.innerText = todoInput.value;
+
+  const doneItem = document.createElement("i");
+  doneItem.classList.add("fa-solid");
+  doneItem.classList.add("fa-check");
+
+  doneItem.addEventListener("click", () => handleDoneClick(taskContent));
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemContainer, taskContent)
+  );
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(doneItem);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
 
   todoInput.value = "";
+  todoInput.focus();
 };
 
-AddButton.addEventListener("submit", (e) => {
-  e.preventDefault();
+const handleDoneClick = (doneItem) => {
+  const tasks = tasksContainer.childNodes;
 
-  const inputValue = todoInput.value;
+  for (const task of tasks) {
+    if (task.firstChild.isSameNode(doneItem)) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+};
 
-  if (inputValue) {
-    saveTodo(inputValue);
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    if (task.firstChild.isSameNode(taskContent)) {
+      taskItemContainer.remove();
+    }
+  }
+};
+
+AddButton.addEventListener("click", () => {
+  handleAddTask();
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const keyEnter = document.querySelector("#submit");
+
+    keyEnter.click();
   }
 });
